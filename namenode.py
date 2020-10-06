0393ef1468fb5bad1faebad635807db6c63f459b
Yusuf_Roshdy <yusufroshdy9@gmail.com>
2020-10-06 20:39:48 +0300

    Added mkdir function

--- namenode.py
+++ namenode.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-tree = {}
+tree = {'./': 1}
 
 def save_tree():
     with open('tree.pkl', 'wb') as f:
@@ -22,11 +22,19 @@ def load_tree():
         tree =  pickle.load(f)
 load_tree()
 
+def format_path(path):
+    if path[:2] != './':
+        path = './' + path
+    if path[-1] != '/':
+        path = path + '/'
+    return path
+
+
 @app.route('/ls',methods = ['GET'])
 def ls():
     path = request.form['path']
-    if path[-1] != '/':
-        path = path + '/'
+    path = format_path(path)
+
     ret = []
     if path not in tree.keys():
         return path + ' is not a directory'
@@ -34,6 +42,24 @@ def ls():
         if path in t and len(path) != len(t):
             ret.append(t[len(path):])
     return '\n'.join(ret)
+
+@app.route('/mkdir',methods = ['POST'])
+def mkdir():
+    global tree
+    path = request.form['path']
+    
+    path = format_path(path)
+    print(path)
+
+    if path in tree.keys():
+        return path + ' already exist'
+    names = path.split('/')
+    print(names)
+    print(tree)
+    if '/'.join([*names[:-2], '']) not in tree.keys():
+        return "Parent directory doesn't exist"
+    tree[path] = 1
+    return 'sucsess'
 
 if __name__ == '__main__':
    app.run(host=sys.argv[1], port=int(sys.argv[2]), debug = True)
