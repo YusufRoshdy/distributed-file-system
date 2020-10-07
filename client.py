eb923f2117d17177c2c2a14a5d003d57551e41f3
kalraUtkarsh <kalra.utkarsh@gmail.com>
2020-10-07 23:22:37 +0300

    added initialize and file create mehtods

--- client.py
+++ client.py
@@ -1,3 +1,18 @@
+import requests
+def initialze(html):
+    
+    
+    payload = {'key1': 'initialze'}
+    r=requests.post(html,data=payload)
+    return r
+def file_create(html):
+    print('Enter the file name you want to create:')
+    file=input().lower()
+    payload = {'key1': 'file_create', 'key2': file}
+    r = requests.post(html, data=payload)
+    return r
+
+    
 def main():
     print('Welcome Client!')
     print('what do you want to do?')
