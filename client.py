2130fcb67f0e7d7676491abc435efedbc910283c
kalraUtkarsh <kalra.utkarsh@gmail.com>
2020-10-07 23:23:47 +0300

    added file_write and file_read mehtods

--- client.py
+++ client.py
@@ -12,7 +12,19 @@ def file_create(html):
     r = requests.post(html, data=payload)
     return r
 
-    
+ def file_read(html):
+    print('Enter the file name you want to read:')
+    file = input().lower()
+    payload = {'key1': 'file_read', 'key2': file}
+
+    r1 = requests.get(html, params=payload)
+    return r1.content
+def file_write(html):
+    print('Enter the file name you want to write to dfs(should be in the same folder):')
+    file = input().lower()
+    file1=open(file)
+    payload = {'key1': 'file_write', 'key2': [file, file1]}
+    r1 = requests.put(html, data=payload)   
 def main():
     print('Welcome Client!')
     print('what do you want to do?')
