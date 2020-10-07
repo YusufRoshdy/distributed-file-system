c355a4df42723f9e3965d72c748d03744b4d67cb
Yusuf_Roshdy <yusufroshdy9@gmail.com>
2020-10-07 07:27:01 +0300

    Added touch function and fix bugs in ls

--- namenode.py
+++ namenode.py
@@ -2,63 +2,59 @@ from flask import Flask, redirect, url_for, request
 import sys
 import pickle
 import os
+from utilities import *
 
 app = Flask(__name__)
 
-tree = {'./': 1}
-
-def save_tree():
-    with open('tree.pkl', 'wb') as f:
-        pickle.dump(tree, f, pickle.HIGHEST_PROTOCOL)
-
-def load_tree():
-    global tree
-    if not os.path.isfile('tree.pkl'):
-        print('Initializeing tree')
-        with open('tree.pkl', 'wb') as f:
-            pickle.dump(tree, f, pickle.HIGHEST_PROTOCOL)
-    with open('tree.pkl', 'rb') as f:
-        # print(pickle.load(f))
-        tree =  pickle.load(f)
-load_tree()
-
-def format_path(path):
-    if path[:2] != './':
-        path = './' + path
-    if path[-1] != '/':
-        path = path + '/'
-    return path
-
+tree = load_tree()
 
 @app.route('/ls',methods = ['GET'])
 def ls():
     path = request.form['path']
-    path = format_path(path)
+    path = format_path(path, 'folder')
 
     ret = []
     if path not in tree.keys():
         return path + ' is not a directory'
     for t in tree:
         if path in t and len(path) != len(t):
-            ret.append(t[len(path):])
+            if t[len(path):][-1] == '/' or '/' not in t[len(path):]:
+                ret.append(t[len(path):])
     return '\n'.join(ret)
 
-@app.route('/mkdir',methods = ['POST'])
+@app.route('/mkdir',methods = ['PUT'])
 def mkdir():
     global tree
     path = request.form['path']
     
-    path = format_path(path)
-    print(path)
+    path = format_path(path, 'folder')
 
     if path in tree.keys():
         return path + ' already exist'
-    names = path.split('/')
-    print(names)
-    print(tree)
-    if '/'.join([*names[:-2], '']) not in tree.keys():
+    
+    if not check_parent_exist(path, tree):
         return "Parent directory doesn't exist"
+
     tree[path] = 1
+    save_tree(tree)
+    # TODO: send the command to the servers
+    return 'sucsess'
+
+@app.route('/touch',methods = ['PUT'])
+def touch():
+    global tree
+    path = request.form['path']
+    path = format_path(path, 'file')
+
+    if path in tree.keys():
+        return path + ' already exist'
+
+    if not check_parent_exist(path, tree):
+        return "Parent directory doesn't exist"
+    
+    tree[path] = 1
+    save_tree(tree)
+    # TODO: send the command to the servers
     return 'sucsess'
 
 if __name__ == '__main__':
---
+++ utilities.py
@@ -0,0 +1,35 @@
+import sys
+import pickle
+import os
+
+def save_tree(tree):
+    with open('tree.pkl', 'wb') as f:
+        pickle.dump(tree, f, pickle.HIGHEST_PROTOCOL)
+
+def load_tree():
+    tree = {'./': 1}
+    if not os.path.isfile('tree.pkl'):
+        print('Initializeing tree')
+        with open('tree.pkl', 'wb') as f:
+            pickle.dump(tree, f, pickle.HIGHEST_PROTOCOL)
+    with open('tree.pkl', 'rb') as f:
+        # print(pickle.load(f))
+        tree =  pickle.load(f)
+    return tree
+load_tree()
+
+def format_path(path, path_type='folder'):
+    if path[:2] != './':
+        path = './' + path
+    if path[-1] != '/' and path_type == 'folder':
+        path = path + '/'
+    return path
+
+
+def check_parent_exist(path, tree):
+    for i in range(len(path)-2, 0, -1):
+        if path[i] == '/':
+            break
+    if path[:i+1] in tree.keys():
+        return True
+    return False
