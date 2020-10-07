2505b1b5029b1a2b820ce93b0a34cedefbcdcd32
Yusuf_Roshdy <yusufroshdy9@gmail.com>
2020-10-07 08:22:03 +0300

    Added write file function

--- namenode.py
+++ namenode.py
@@ -52,10 +52,32 @@ def touch():
     if not check_parent_exist(path, tree):
         return "Parent directory doesn't exist"
     
+    # TODO: send the command to the servers and edit tree[path] = [??]
     tree[path] = 1
     save_tree(tree)
-    # TODO: send the command to the servers
     return 'sucsess'
+
+
+@app.route('/write',methods = ['PUT'])
+def write():
+    global tree
+    path = request.form['path']
+    path = format_path(path, 'folder')
+
+    if path not in tree.keys():
+        return "Directory doesn't exist"
+    if 'file' not in request.files:
+        return 'No file received'
+    file = request.files['file']
+    print(file)
+    filename = file.filename
+    file.save(filename)
+    # TODO: send the command to the servers
+    tree[path+filename] = 1
+    save_tree(tree)
+
+    return 'sucsess'
+
 
 if __name__ == '__main__':
    app.run(host=sys.argv[1], port=int(sys.argv[2]), debug = True)
