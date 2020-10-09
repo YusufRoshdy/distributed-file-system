da8fa1e37cf62e930f30cca4674cf4f51132f141
kalraUtkarsh <kalra.utkarsh@gmail.com>
2020-10-09 22:54:08 +0300

    fixed some bugs and tested

--- Client/client.py
+++ Client/client.py
@@ -3,104 +3,132 @@ cur_dir=''
 
 def initialze(html, X, cur_dir):
 
-    ip = html + '/' + 'initialze'
+    ip = html + 'initialze'
     payload = {'key1': 'initialze'}
     r=requests.post(ip)
+    print('initialzed')
     return cur_dir
 
 #here the input is like "mkf file.txt"
 def file_create(html, X, cur_dir):
-    ip = html + '/'+'mkf'
-
-    payload = {'path': cur_dir, 'file': X[1]}
-    r = requests.post(ip, data=payload)
+    ip = html +'touch'
+    if(len(X)<2):
+        print('give the name of the file you want to create')
+        return
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
+    payload = {'path': path}
+    r = requests.put(ip, data=payload)
+    print('file created')
     return cur_dir
 
 #here the input is like "cat file.txt"
 def file_read(html, X, cur_dir):
-    ip = html + '/'+ 'cat'
+    ip = html + 'cat'
     path = cur_dir + '/' + X[1]
+    print(path)
     payload = {'path': path}
     r1 = requests.get(ip, params=payload)
-    return r1.files, cur_dir
+    #open(X[1], 'wb').write(r1.content)
+    print('read complete')
+    return r1, cur_dir
 
-# this file write i dont know
-def file_write(html):
+# the input is like fw file.txt
+def file_write(html, X, cur_dir):
 
-    file = input().lower()
-    file1=open(file)
-    payload = {'key1': 'file_write', 'key2': [file, file1]}
+    ip = html + 'fw'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir
+    #file1=open(X[1])
+    file1=X[1]
+    payload = {'path':[path,file1]}
     r1 = requests.put(html, data=payload)
-
+    print('wrote')
+    return cur_dir
 #here the input is like "rm file.txt"
+
 def file_delete(html, X, cur_dir):
-    ip = html + '/' + 'rm'
-    path = cur_dir + '/' + X[1]
+    ip = html + 'rm'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
     payload = {'path': path}
     r1 = requests.delete(ip, data=payload)
+    print('deleted')
     return cur_dir
 
 #not sure tozhe
-def file_info(html):
-    print('Enter the name if the file you want to get the information about:')
-    file = input().lower()
-    payload = {'key1': 'file_info', 'key2': file}
-    r1 = requests.get(html, data=payload)
-    return r1.content
+def file_info(html, X, cur_dir):
+    ip = html + 'info'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
+    payload = {'key1': path,}
+    r1 = requests.get(ip, data=payload)
+    print('infoed')
+    #print(str(r1.content))
+    return cur_dir
 
 # here the input is like "fcp file.txt"
 def file_copy(html, X, cur_dir):
-    path = cur_dir + '/' + X[1]
-    ip = html + '/' + 'fcp'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
+    ip = html + 'fcp'
     payload = {'path': path}
     r1 = requests.post(ip, data=payload)
+    print('copied')
     return cur_dir
 
 # here the input is like "fm file.txt directory"
 def file_move(html, X, cur_dir):
 
-    ip = html + '/' + 'fm'
-    path = cur_dir + '/' + X[1]
+    ip = html + 'fm'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
     dir = ''
-    if (X[2][1] != '/'):
+    if (X[2][0] != '/'):
         dir = '/' + X[2]
     else:
         dir = X[2]
     payload = {'path': [path, dir]}
-    r=requests.patch(html, data=payload)
+    r=requests.patch(ip, data=payload)
+
+    print('moved')
     return cur_dir
 
 #here the input is like "cd directory"
 def open_directory(html,X, cur_dir):
-    ip = html + '/' + 'ls'
-    if(X[1][1]!='/'): X[1]='/'+X[1]
-    path = cur_dir + '/' + X[1]
+    ip = html + 'ls'
+    if(X[1][0]!='/'): X[1]='/'+X[1]
+    path = cur_dir + X[1]
     payload = {'path': path}
     r = requests.post(ip, data=payload)
     if(r.content=='empty'):
         print('this directory does not exist')
+    print('changed directory')
     return path
 
 #here the input is like "ls directory"
 def read_directory(html,X, cur_dir):
     dir=''
-    if(X.len>1):
-        if (X[1][1] != '/'): X[1] = '/' + X[1]
+    if(len(X)>1):
+        if (X[1][0] != '/'): X[1] = '/' + X[1]
         dir = X[1]
-    ip=html + '/ls'
+    ip=html + 'ls'
     path= cur_dir + '/' + dir
-    dir= input().lower
+
     payload = {'path': path}
-    r=requests.get(html, data=payload)
+    r=requests.get(ip, data=payload)
+    print('sent ls')
     return dir
 
 #here the input is like "mkdir directory"
 def make_directory(html, X, cur_dir):
 
-    ip  = html + '/' + 'mkdir'
-    path = cur_dir + '/' + X[1]
-    payload = {'path': 'make_directory'}
-    r = requests.post(ip, data=payload)
+    ip  = html +  'mkdir'
+    if (X[1][0] != '/'): X[1] = '/' + X[1]
+    path = cur_dir + X[1]
+    payload = {'path': path}
+    r = requests.put(ip, data=payload)
+    print('made directory')
     return cur_dir
 
 #this i am not sure
@@ -122,36 +150,39 @@ def delete_directory(html, X, cur_dir):
 
 def get_input(html, X, cur_dir):
 
-    if(X == 'initialze'):
+    if(X[0] == 'initialize'):
         cur_dir=initialze(html, X, cur_dir)
 
-    if(X[0]== 'mkf'):
+    if(X[0]== 'touch'):
         cur_dir=file_create(html, X, cur_dir)
     if (X[0] == 'cat'):
         cur_dir=file_read(html, X, cur_dir)[1]
-    if(X[0]=='nano'):
+    if(X[0]=='fw'):
         cur_dir = file_write(html, X, cur_dir)
 
-    if (X == 'rmf'):
+    if (X[0] == 'rm'):
         cur_dir =file_delete(html, X, cur_dir)
-    if (X == 'file info'):
+    if (X[0] == 'info'):
         cur_dir =file_info(html, X, cur_dir)
-    if (X == 'fcp'):
+    if (X[0] == 'fcp'):
         cur_dir =file_copy(html, X, cur_dir)
-    if (X == 'fm'):
+    if (X[0] == 'fm'):
         cur_dir =file_move(html, X, cur_dir)
-    if (X == 'cd'):
+    if (X[0] == 'cd'):
         cur_dir =open_directory(html, X, cur_dir)
-    if (X == 'ls'):
+    if (X[0] == 'ls'):
 
         cur_dir = read_directory(html, X, cur_dir)
-    if (X == 'mkdir'):
+    if (X[0] == 'mkdir'):
         cur_dir =make_directory(html, X, cur_dir)
-    if (X == 'rmdir'):
+    if (X[0] == 'rmdir'):
         cur_dir =delete_directory(html, X, cur_dir)
     return cur_dir
 
 def main(cur_dir, ip):
      X=input()
-     X.split()
+     X=X.split(" ")
+     print(X)
      cur_dir=get_input(ip, X, cur_dir)
+     print(cur_dir)
+main('.','http://localhost:23/')
