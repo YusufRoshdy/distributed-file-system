95f02c87ef506d3d52e54c86180bab8a8a6f667b
kalraUtkarsh <kalra.utkarsh@gmail.com>
2020-10-09 17:24:22 +0300

    changed some schema of the client, still working on it.

--- Client/client.py
+++ Client/client.py
@@ -1,35 +1,39 @@
 import requests
+cur_dir=''
 def initialze(html):
-    
-    
+
+
     payload = {'key1': 'initialze'}
     r=requests.post(html,data=payload)
     return r
-def file_create(html):
-    print('Enter the file name you want to create:')
-    file=input().lower()
-    payload = {'key1': 'file_create', 'key2': file}
-    r = requests.post(html, data=payload)
-    return r
+def file_create(html, X, cur_dir):
+    ip = html + '/'+'mkfile'
 
- def file_read(html):
-    print('Enter the file name you want to read:')
-    file = input().lower()
-    payload = {'key1': 'file_read', 'key2': file}
+    payload = {'path': cur_dir, 'file': X[1]}
+    r = requests.post(ip, data=payload)
 
-    r1 = requests.get(html, params=payload)
-    return r1.content
+
+def file_read(html, X, cur_dir):
+    ip = html + '/'+ 'cat'
+    path = cur_dir + '/' + X[1]
+    payload = {'path': path}
+    r1 = requests.get(ip, params=payload)
+    return r1.files
+
+# this file write i dont know
 def file_write(html):
-    print('Enter the file name you want to write to dfs(should be in the same folder):')
+
     file = input().lower()
     file1=open(file)
     payload = {'key1': 'file_write', 'key2': [file, file1]}
     r1 = requests.put(html, data=payload)
-def file_delete(html):
-    print('Enter the name of the file you want to delete:')
-    file = input().lower()
-    payload = {'key1': 'file_delete', 'key2': file}
-    r1 = requests.delete(html, data=payload)
+
+def file_delete(html, X, cur_dir):
+    ip = html + '/' + 'rm'
+    path = cur_dir + '/' + X[1]
+    payload = {'path': path}
+    r1 = requests.delete(ip, data=payload)
+    return cur_dir
 def file_info(html):
     print('Enter the name if the file you want to get the information about:')
     file = input().lower()
@@ -48,24 +52,32 @@ def file_move(html):
     print('Enter the path seperated by "/" where you want to put the file:')
     path = input().lower()
     payload = {'key1': 'file_move', 'key2': [file,path]}
-    r=requests.patch(html, data=payload)   
- def open_directory(html):
-    print('Enter the name of the directory to open:')
-    file = input().lower()
+    r=requests.patch(html, data=payload)
+def open_directory(html,X, cur_dir):
+    ip = html + '/' + 'ls'
+    path = cur_dir + '/' + X[1]
+    payload = {'path': path}
+    r = requests.post(ip, data=payload)
+    if(r.content=='empty'):
+        print('this directory does not exist')
 
-    payload = {'key1': 'open_directory', 'key2': file}
-    r = requests.post(html, data=payload)
-def read_directory(html):
-    print('Enter the name of the directory to read:')
+def read_directory(html,X, cur_dir):
+    dir=''
+    if(X.len>1):
+        dir = X[1]
+    ip=html + '/ls'
+    path= cur_dir + '/' + dir
     dir= input().lower
-    payload = {'key1': 'read_directory', 'key2':dir}
+    payload = {'path': path}
     r=requests.get(html, data=payload)
-    return r.content
-def make_directory(html):
-    print('Enter the name of the directory to make:')
-    dir = input().lower
-    payload = {'key1': 'make_directory', 'key2': dir}
-    r = requests.post(html, data=payload)
+    return dir
+def make_directory(html, X, cur_dir):
+
+    ip  = html + '/' + 'mkdir'
+    path = cur_dir + '/' + X[1]
+    payload = {'path': 'make_directory'}
+    r = requests.post(ip, data=payload)
+
 
 def delete_directory(html):
     print('Enter the name of the directory to delete:')
@@ -81,10 +93,11 @@ def delete_directory(html):
             r=requests.delete(html, data=payload)
         else:
             pass
-def get_input(html, X):
+def get_input(html, X, cur_dir):
 
     if(X == 'initialze'):
-        initialze(html)
+        cur_dir=initialze(html, X, cur_dir)
+
     if(X== 'file create'):
         file_create(html)
     if (X == 'file read'):
@@ -100,17 +113,18 @@ def get_input(html, X):
         file_copy(html)
     if (X == 'file move'):
         file_move(html)
-    if (X == 'open directory'):
+    if (X == 'ls'):
         open_directory(html)
-    if (X == 'read directory'):
-        read_directory(html)
+    if (X == 'ls'):
+
+        cur_dir = read_directory(html, X, cur_dir)
     if (X == 'make directory'):
         make_directory(html)
     if (X == 'delete directory'):
         delete_directory(html)
-               
-def main():
-    print('Welcome Client!')
-    print('what do you want to do?')
-    print('Your options are:')
-    print('''thifss is''')
+    return cur_dir
+
+def main(cur_dir, ip):
+     X=input()
+     X.split()
+     get_input(ip, X, cur_dir)
